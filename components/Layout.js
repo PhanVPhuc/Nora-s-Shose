2989fd5cdda96ba617b882eaa035b74f02076662
Phan_Dinh Vinh_Phuc <pphuc1996@gmail.com>
2022-12-08 16:32:03 +0700

    update

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,22 @@
-import Nav from "./Nav";
+import React from 'react';
+import Head from 'next/head';
 
-export default function Layout({children})
-{
-    return(
-        <div>
-            <Nav />
-            <main>
-                {children}
-            </main>
-        </div>
-    )
+export default function Layout({ title, Children }) {
+  return (
+    <>
+      <Head>
+        <title> {title ? title + ' - Nora' : 'Nora Shoes'} </title>
+        <meta name="description" content="Nora Shose Store" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <header>header</header>
+
+        <main>{Children} </main>
+
+        <footer>footer</footer>
+      </div>
+    </>
+  );
 }
