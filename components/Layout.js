584797383a8bd9749c708c1fa4b053919993e1aa
Phan_Dinh Vinh_Phuc <pphuc1996@gmail.com>
2022-12-08 15:22:05 +0700

    first-day-creating-this-website

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+
+export default function Layout({children})
+{
+    return(
+        <div>
+            <Nav />
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav()
+{
+    return
+    (
+        <div> 
+            <h1  >
+                Nav
+            </h1>
+        </div>
+    )
+}
